1e2dc8dc845a09afb5f999e22e2a035ca36f520d
Yuqing Huang <670081066@qq.com>
2024-02-22 20:06:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #cecho "****************** Installing pytorch ******************"
-#conda install pytorch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0 cudatoolkit=10.2 -c pytorch
+conda install pytorch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0 cudatoolkit=10.2 -c pytorch
 
 
 echo ""
